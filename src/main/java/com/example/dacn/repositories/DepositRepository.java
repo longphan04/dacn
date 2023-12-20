138913e9a6de9ce9607de417b2bf1c533efe1df2
Long Phan <phanvietlong04@gmail.com>
2023-12-20 21:29:29 +0700

    fixbug

--- src/main/java/com/example/dacn/repositories/DepositRepository.java
+++ src/main/java/com/example/dacn/repositories/DepositRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepositRepository extends JpaRepository<Deposit, Integer> {
-   @Query(value = "select sum(amount) from deposit inner join users on users.id = deposit.user_id where username = ?1 and year(date) = ?2 and month(date) = ?3 and day(date) = ?4", nativeQuery = true)
+   @Query(value = "select sum(amount) from deposits inner join users on users.id = deposits.user_id where username = ?1 and year(date) = ?2 and month(date) = ?3 and day(date) = ?4", nativeQuery = true)
     Double getDepositsInYear(@Param("username") String username, @Param("year") int year, @Param("month") int month, @Param("day") int day);
 }
