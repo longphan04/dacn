045d080f8183f256d83a41069016ec572daad5a5
Viet Phan <viet.phanquoc@axonactive.com>
2023-12-26 18:29:42 +0700

    implement new UI for finance screen

--- src/main/java/com/example/dacn/controllers/FinanceController.java
+++ src/main/java/com/example/dacn/controllers/FinanceController.java
@@ -7,6 +7,7 @@ import com.example.dacn.services.WithdrawService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +19,27 @@ public class FinanceController {
     private final DepositService depositService;
     private final WithdrawService withdrawService;
 
+    @GetMapping("deposit")
+    public String depositIndex() {
+        return "deposit";
+    }
+
+    @GetMapping("withdraw")
+    public String withdrawIndex() {
+        return "withdraw";
+    }
+
     @PostMapping("deposit")
     public String deposit(@ModelAttribute DepositDTO depositDTO, Model model) {
         DepositDTO deposit = depositService.save(depositDTO);
         model.addAttribute("deposit", deposit);
-        return "deposit";
+        return "home";
     }
 
     @PostMapping("withdraw")
     public String withdraw(@ModelAttribute WithdrawDTO withdrawDTO, Model model) {
         WithdrawDTO withdraw = withdrawService.save(withdrawDTO);
         model.addAttribute("withdraw", withdraw);
-        return "withdraw";
+        return "home";
     }
 }
